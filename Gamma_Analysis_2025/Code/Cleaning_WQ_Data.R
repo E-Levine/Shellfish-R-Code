c8321f37d6b6f701a87e3dbced93bfb38cc010a9
secahillmoore <176531754+secahillmoore@users.noreply.github.com>
2025-03-20 10:47:24 -0400

    creating .csv for cleaned WQ dataset

--- Gamma_Analysis_2025/Code/Cleaning_WQ_Data.R
+++ Gamma_Analysis_2025/Code/Cleaning_WQ_Data.R
@@ -8,17 +8,17 @@ library(dplyr)
 library(lubridate)
 #
 ##Set Estuary working within, and start and end years of data. 
-Est <- c("TB") #Enter two-letter Estuary code.
-Start_year <- c("2005") #Year the dataset will begin (varies by bay)
-End_year <- c("2024") #Year the dataset ends 
-SampleEv <- c("RCRT") #4 letter Sample Event Code
+Est <- c("AB") #Enter two-letter Estuary code.
+Start_year <- c("2016") #Year the dataset will begin (varies by bay)
+End_year <- c("2023") #Year the dataset ends 
+SampleEv <- c("COLL") #4 letter Sample Event Code
 #
 ## Make sure datasets are saved in .csv format
 ##### Load datasets
-WQ <-read.csv("Raw_data/SampleEventWQ.csv")
+WQ <-read.csv("Gamma_Analysis_2025/Data/Raw_data/SampleEventWQ.csv")
 View(WQ)
 #
-FixedLoc <- read.csv("Raw_data/FixedLocations.csv")
+FixedLoc <- read.csv("Gamma_Analysis_2025/Data/Raw_data/FixedLocations.csv")
 #
 # Extract year, month, and day, add it to WQ df 
 WQ <- WQ %>% 
@@ -68,5 +68,5 @@ WQ <- WQ %>%
 
 #Save to .csv
 write.csv(WQ, 
-          file = paste0("Clean_data/",Est,"_filtered_WQ_", SampleEv, "_", Start_year, "_", End_year,".csv"), 
+          file = paste0("Gamma_Analysis_2025/Data/Cleaned_data/",Est,"_filtered_WQ_", SampleEv, "_", Start_year, "_", End_year,".csv"), 
           row.names = FALSE)
