a20ea53587283245cfea98bfd7698d8d5953e4fe
secahillmoore <176531754+secahillmoore@users.noreply.github.com>
2025-03-31 17:14:00 -0400

    moved plots to prep script

--- Gamma_Analysis_2025/Code/AB_CI_Gamma.R
+++ Gamma_Analysis_2025/Code/AB_CI_Gamma.R
@@ -19,10 +19,16 @@ library(ggh4x)
 #### WQ data is included for all years
 AB.CIwq <-read.csv('Data/Cleaned_Data/AB_CIwq_2016_2023.csv', skip = 0, na= "Z")
 View(AB.CIwq)
-
-
-### on to the Gamma analysis ###
+#
+## Need to rerun the mGamma code again. Because mGamma.A had lowest AIC score, we will only use that one moving forward for comparisons
 ## Scale WQ parameters
+str(AB.CIwq)
+AB.CIwq$CI <- as.numeric(AB.CIwq$CI)
+AB.CIwq$Temperature <- as.numeric(AB.CIwq$Temperature)
+AB.CIwq$Salinity <- as.numeric(AB.CIwq$Salinity)
+AB.CIwq$DO <- as.numeric(AB.CIwq$DO)
+AB.CIwq$pH <- as.numeric(AB.CIwq$pH)
+
 scDO <- scale(AB.CIwq$DO)
 scTemp <- scale(AB.CIwq$Temperature)
 scSalinity <- scale(AB.CIwq$Salinity)
@@ -39,72 +45,6 @@ mGamma.A <- glmmTMB(CI ~ Temperature + Salinity + DO + pH + GroupYear + (1|GYD),
                     dispformula = ~ GroupYear,
                     family = Gamma(link = "log"), 
                     data = AB.CIwq)
-
-##### Assess goodness-of-fit 
-simulateResiduals(mGamma.A, n = 1000, plot = T)
-
-##### Look at estimates
-summary(mGamma.A)
-
-## Varying one variable at a time; Station and Date = NA to make population-level predictions (i.e., predictions that ignore the random effects subjects)
-newDat <- expand.grid(GroupYear = unique(AB.CIwq$GroupYear), Temperature = unique(AB.CIwq$Temperature, na.rm = T), 
-                      Salinity = mean(AB.CIwq$Salinity, na.rm = T), DO = mean(AB.CIwq$DO, na.rm = T), 
-                      pH = mean(AB.CIwq$pH, na.rm = T), Station = NA, Date = NA, GYD = NA) #Again, should Station be Region/Section instead?
-predsGamma <- predict(mGamma.A, newdata = newDat, type = "link", se.fit = T)
-preddsGamma <- data.frame(newDat, fit = predsGamma$fit, se = predsGamma$se.fit)
-preddsGamma$mean <- exp(preddsGamma$fit)
-preddsGamma$lwr <- exp(preddsGamma$fit - 1.96*preddsGamma$se)
-preddsGamma$upr <- exp(preddsGamma$fit + 1.96*preddsGamma$se)
-preddsGamma <- preddsGamma %>% separate(GroupYear, into = c("Region", "Group", "Year"), remove = FALSE) %>%
-  mutate(LocationGroup = paste0(Region," ", Group))
-
-desired_order <- c("West Section", "Central Section", "East Section")
-preddsGamma$LocationGroup <- factor(preddsGamma$LocationGroup, levels = desired_order)
-preddsGamma$pre.post <- ifelse(preddsGamma$Year < 2020, "pre", "post")
-preddsGamma$Yearn<- as.numeric(preddsGamma$Year)
-
-## Adding customized colors for years and include designated section colors to match the map and scatter plots above
-strip <- strip_themed(background_x = elem_list_rect(fill = Section.colors))
-
-bar.colors <- c("2016"= "#FFBBBB",
-                "2017"="#56B4E9",
-                "2018"="#CC79A7",
-                "2019"="#FF7000",
-                "2021"= "#0067A0",
-                "2022"="#94DCC8",
-                "2023"="grey")
-
-(gammaPlot.a <- ggplot(preddsGamma, aes(x = Temperature, y = mean, fill = LocationGroup)) +
-    geom_line() + 
-    geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.25) + 
-    facet_wrap(~Year) + 
-    theme_bw() + 
-    theme(axis.text = element_text(color = "black"), 
-          panel.grid.major.y = element_line(colour = "grey90"), 
-          panel.grid.minor.y = element_line(colour = "grey90", linetype = "dashed"), 
-          panel.grid.major.x = element_blank(), legend.title = element_blank(), legend.position = "bottom") + 
-    scale_fill_manual(values = Section.colors)+
-    scale_y_continuous(limits = c(0, 5), 
-                       breaks = seq(0, 5, 0.5), expand = expansion(add = c(0, 0))) + 
-    labs(x = "Mean temperature (°C)", y = "Mean condition index (± 95% CL)", title = "Condition Index of Eastern Oysters in Apalachicola Bay 2016-2023"))
-
-
-##### Summarize the raw data (useful to compare to model estimates)
-c_summ <- AB.CI %>% group_by(GroupYear) %>% summarise(N = n(), mn = mean(CI, na.rm = T), se = sd(CI, na.rm = T)/sqrt(N), lwr = mn-1.96*se, upr = mn+1.96*se) %>% separate(GroupYear, into = c("Section", "Group", "Year"), remove = FALSE) %>% mutate(LocationGroup = paste0(Section," ", Group))
-desired.order <- c("West", "Central", "East")
-c_summ$LocationGroup <- factor(c_summ$LocationGroup, levels = desired_order)
-c_summ <- c_summ %>% arrange(LocationGroup, Year)
-
-##### Compare estimated/predicted means to those observed in the raw data: 
-(rawdataPlot <- ggplot(c_summ, aes(x = Year, y = mn, fill = Year)) + 
-    geom_bar(stat = "identity", color = "black") + geom_errorbar(aes(ymin = lwr, ymax = upr), width = 0.25) + 
-    facet_wrap(~LocationGroup) + theme_bw() + theme(axis.text = element_text(color = "black"), 
-                                                    panel.grid.major.y = element_line(colour = "grey90"), 
-                                                    panel.grid.minor.y = element_line(colour = "grey90", linetype = "dashed"), 
-                                                    panel.grid.major.x = element_blank()) + 
-    scale_y_continuous(limits = c(0,4), breaks = seq(0,4, 0.25), expand = expansion(add = c(0,0))) + 
-    labs(x = NULL, y = "Raw data: Mean Condition Index"))
-#### does not match
 
 ##### Compare means among years for each Section
 (emmGamma <- data.frame(emmeans(mGamma.A, specs = pairwise ~ GroupYear)$emmeans)) # these are marginal or group-specific means
